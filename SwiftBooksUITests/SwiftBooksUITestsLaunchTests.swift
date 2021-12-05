f0d0e4f80418fc1e623011d649f414a8749622c1
emirisama <emirin.ayu.vn@gmail.com>
2021-12-05 16:52:18 +0900

    Initial Commit

---
+++ SwiftBooksUITests/SwiftBooksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftBooksUITestsLaunchTests.swift
+//  SwiftBooksUITests
+//
+//  Created by 中森えみり on 2021/12/05.
+//
+
+import XCTest
+
+class SwiftBooksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
